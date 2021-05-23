dbd914ddde96fbe96a46a84c6317328ad08a8ad3
morti <bsarioglo@gmail.com>
2021-05-23 14:40:31 +0300

    [Issue 12.5] Database connector
    - Added basic connector to remote database

---
+++ database.py
@@ -0,0 +1,19 @@
+import mysql.connector
+import sys
+
+cnx = mysql.connector.connect(user="rfamro",
+                                host="mysql-rfam-public.ebi.ac.uk",
+                                port="4497",
+                                database="Rfam")
+                                  
+cursor = cnx.cursor()
+query = "SELECT fr.rfam_acc, fr.rfamseq_acc, fr.seq_start, fr.seq_end FROM full_region fr, rfamseq rf, taxonomy tx WHERE rf.ncbi_id = tx.ncbi_id AND fr.rfamseq_acc = rf.rfamseq_acc AND tx.ncbi_id = 10116  AND is_significant = 1"
+
+cursor.execute(query)
+result = cursor.fetchall()
+for row in result:
+        print(row)
+
+cursor.close()
+cnx.close()
+
